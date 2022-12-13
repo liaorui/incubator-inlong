7770bdd9b29679a141e947b7235bb5691cde5e97
ryanrliao <ryanrliao@tencent.com>
2022-12-13 17:22:23 +0800

    mysql all migration incrementalSnapshotEnabled set true

--- inlong-manager/manager-pojo/src/main/java/org/apache/inlong/manager/pojo/sort/util/ExtractNodeUtils.java
+++ inlong-manager/manager-pojo/src/main/java/org/apache/inlong/manager/pojo/sort/util/ExtractNodeUtils.java
@@ -231,7 +231,7 @@ public class ExtractNodeUtils {
         Map<String, String> properties = parseProperties(binlogSource.getProperties());
         if (binlogSource.isAllMigration()) {
             // Unique properties when migrate all tables in database
-            incrementalSnapshotEnabled = false;
+            //incrementalSnapshotEnabled = false;
             properties.put("migrate-all", "true");
         }
         if (StringUtils.isEmpty(primaryKey)) {
